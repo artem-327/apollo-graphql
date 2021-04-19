4eab073ef110e41a84a85d156958134885dca79d
Kiet <72509022+wwwKiet@users.noreply.github.com>
2021-04-19 17:40:59 +0300

    App client update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
 import Routes from './Routes';
 import LanguageIcon from '@material-ui/icons/Language';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import React from "react";
+
+const client = new ApolloClient({
+    uri: "https://71z1g.sse.codesandbox.io/",
+    cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <div className="float-left">
-          <a className="logo" href="/">
-            LoanAid.AI
-          </a>
-        </div>
-        <div className="float-right">
-          <button className="btn btn-green">LOGIN</button>
-          <LanguageIcon className="mr-2" /> 
-          <span className="mr-5"> En </span>
-        </div>
-      </div>
-      <main>
-        <Routes />
-      </main>
-    </div>
+      <ApolloProvider client={client}>
+            <div className="App">
+              <div className="App-header">
+                <div className="float-left">
+                  <a className="logo" href="/">
+                    LoanAid.AI
+                  </a>
+                </div>
+                <div className="float-right">
+                  <button className="btn btn-green">LOGIN</button>
+                  <LanguageIcon className="mr-2" />
+                  <span className="mr-5"> En </span>
+                </div>
+              </div>
+              <main>
+                <Routes />
+              </main>
+            </div>
+      </ApolloProvider>
   );
 }
 
